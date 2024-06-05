fb3ade7c8773c4b7ff72bae515af6c00f3471c9c
hjh010806 <010806hjh@naver.com>
2024-06-05 17:43:19 +0900

    데이터 저장 enum형과, OSType 구분 요청

--- backend/src/main/java/com/team/shopping/ShoppingApplication.java
+++ backend/src/main/java/com/team/shopping/ShoppingApplication.java
@@ -1,13 +1,21 @@
 package com.team.shopping;
 
+import com.team.shopping.Enums.OsType;
+import com.team.shopping.Exceptions.DataNotFoundException;
+import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShoppingApplication {
+	@Getter
+	private static OsType osType;
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShoppingApplication.class, args);
+		osType = OsType.getOsType();
+		if (osType != null)
+			SpringApplication.run(ShoppingApplication.class, args);
+		else
+			throw new DataNotFoundException("없는 OS 입니다.");
 	}
-
 }
