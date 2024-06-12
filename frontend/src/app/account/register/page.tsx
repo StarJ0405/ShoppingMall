4117d839bdd545b3f75fb67fd6a99e8ccec1412c
Hong SeongJae <ghdtjdwo126@naver.com>
2024-06-12 14:02:31 +0900

    프로필 수정 비밀번호 및 이미지 변경제외

--- frontend/src/app/account/register/page.tsx
+++ frontend/src/app/account/register/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { SignUp } from '@/app/API/AuthAPI';
-import { KeyDownCheck, Move, checkInput } from '@/app/Global/Method';
+import { KeyDownCheck, Move, PhoneNumberCheck, checkInput } from '@/app/Global/Method';
 
 import { useState } from 'react'
 
@@ -25,25 +25,9 @@ export default function Page() {
     function IsDisabled() {
         return username == '' || password == '' || name == '' || email == '' || nickname == '' || phoneNumber == '' || role == -1 || birthday == '' || gender == -1;
     }
-    function PhoneNumberCheck(e: any) {
-        const input = e.target as HTMLInputElement;
-        input.value = input.value.replace(/[^0-9]/g, '');
-        if (input.value.length > 3 && input.value.charAt(3) != '-') {
-            const value = input.value;
-            input.value = value.slice(0, 3) + '-' + value.slice(3);
-        }
-        if (input.value.length > 8 && input.value.charAt(8) != '-') {
-            const value = input.value;
-            input.value = value.slice(0, 8) + '-' + value.slice(8);
-        }
-        if(input.value.length>13)
-            input.value = input.value.slice(0,13);
-        if(input.value.lastIndexOf('-') == input.value.length-1)
-            input.value = input.value.slice(0,input.value.length-1);
 
-    }
     function Submit() {
-        SignUp({ username: username, password: password, name: name, email: email, nickname: nickname, phoneNumber: phoneNumber.replaceAll('-', ''), role: role, birthday: birthday, gender: gender })
+        SignUp({ username: username, password: password, name: name, email: email, nickname: nickname, phoneNumber: phoneNumber, role: role, birthday: birthday, gender: gender })
             .then(() => {
                 window.location.href = "/account/login";
             }).catch(error => {
@@ -66,17 +50,17 @@ export default function Page() {
                 <label className='font-bold text-2xl mt-[80px]'>{input_name[focusing]}</label>
                 <label className='font-bold text-sm text-red-500'>{error}</label>
                 <input id='username' type='text' className='w-[400px] text-xl mt-[24px]' autoFocus placeholder='아이디' onFocus={e => { e.target.placeholder = ''; setFocusing(0) }} onBlur={e => e.target.placeholder = '아이디'} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, next: () => Move('password') }); }} onChange={e => setUsername(e.target.value)} maxLength={24} onKeyUp={e => checkInput(e, '^([A-Za-z0-9_]){3,24}$', () => setError(''), () => setError('아이디는 3글자 이상의 영어 대/소문자와 숫자로만 구성 가능합니다.'))} />
-                <input id='password' type='password' className='w-[400px] text-xl mt-[24px]' placeholder='비밀번호' onFocus={e => { e.target.placeholder = ''; setFocusing(1) }} onBlur={e => e.target.placeholder = '비밀번호'} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, pre: () => Move('username'), next: () => Move('email') }) }} onChange={e => setPassword(e.target.value)} maxLength={24} onKeyUp={e => checkInput(e, '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()-+={}~?:;`|/]).{6,24}$', () => setError(''), () => setError('비밀번호는 최소 6, 최대 24자로 대문자, 소문자, 숫자, 특수문자(!@#$%^&*()-+={}~?:;`)가 각각 한개씩 들어가 있어야합니다.'))} />
-                <input id='realname' type='text' className='w-[400px] text-xl mt-[24px]' autoFocus placeholder='이름' onFocus={e => { e.target.placeholder = ''; setFocusing(2) }} onBlur={e => e.target.placeholder = '이름'} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, next: () => Move('password') }); }} onChange={e => setName(e.target.value)} maxLength={5} onKeyUp={e => checkInput(e, '^([가-힣]){2,}$', () => setError(''), () => setError( '이름은 한글 2글자 이상으로 구성되어야 합니다.'))} />
-                <input id='email' type='email' className='w-[400px] text-xl mt-[24px]' placeholder='이메일' onFocus={e => { e.target.placeholder = ''; setFocusing(3) }} onBlur={e => e.target.placeholder = '이메일'} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, pre: () => Move('password'), next: () => Move('nickname') }) }} onChange={e => setEmail(e.target.value)} onKeyUp={e => checkInput(e, '^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', () => setError(''), () => setError('이메일 형식이 맞지 않습니다.'))} />
+                <input id='password' type='password' className='w-[400px] text-xl mt-[24px]' placeholder='비밀번호' onFocus={e => { e.target.placeholder = ''; setFocusing(1) }} onBlur={e => e.target.placeholder = '비밀번호'} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, pre: () => Move('username'), next: () => Move('name') }) }} onChange={e => setPassword(e.target.value)} maxLength={24} onKeyUp={e => checkInput(e, '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()-+={}~?:;`|/]).{6,24}$', () => setError(''), () => setError('비밀번호는 최소 6, 최대 24자로 대문자, 소문자, 숫자, 특수문자(!@#$%^&*()-+={}~?:;`)가 각각 한개씩 들어가 있어야합니다.'))} />
+                <input id='name' type='text' className='w-[400px] text-xl mt-[24px]' autoFocus placeholder='이름' onFocus={e => { e.target.placeholder = ''; setFocusing(2) }} onBlur={e => e.target.placeholder = '이름'} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, pre:()=>Move('password'), next: () => Move('birthday') }); }} onChange={e => setName(e.target.value)} maxLength={5} onKeyUp={e => checkInput(e, '^([가-힣]){2,}$', () => setError(''), () => setError( '이름은 한글 2글자 이상으로 구성되어야 합니다.'))} />
+                <input id='birthday' type='date' className='w-[400px] text-xl mt-[24px]' placeholder='생년월일' onFocus={e => { e.target.placeholder = ''; setFocusing(7) }} onBlur={e => e.target.placeholder = '생년월일'} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, pre: () => Move('name'), next: () => Move('email') }) }} onChange={e => setBirthday(e.target.value)} max={"9999-12-31"}/>
+                <input id='email' type='email' className='w-[400px] text-xl mt-[24px]' placeholder='이메일' onFocus={e => { e.target.placeholder = ''; setFocusing(3) }} onBlur={e => e.target.placeholder = '이메일'} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, pre: () => Move('birthday'), next: () => Move('nickname') }) }} onChange={e => setEmail(e.target.value)} onKeyUp={e => checkInput(e, '^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{3,}$', () => setError(''), () => setError('이메일 형식이 맞지 않습니다.'))} />
                 <input id='nickname' type='text' className='w-[400px] text-xl mt-[24px]' placeholder='닉네임' onFocus={e => { e.target.placeholder = ''; setFocusing(4) }} onBlur={e => e.target.placeholder = '닉네임'} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, pre: () => Move('email'), next: () => Move('phoneNumber') }) }} onChange={e => setNickname(e.target.value)} maxLength={24} onKeyUp={e => checkInput(e, '^[A-Za-z가-힣0-9_\s]{2,24}$', () => setError(''), () => setError('닉네임은 한글,영어,숫자만 가능하며 최대 24자까지 가능합니다.'))} />
-                <input id='phoneNumber' type='text' className='w-[400px] text-xl mt-[24px]' placeholder='전화번호' onFocus={e => { e.target.placeholder = ''; setFocusing(5) }} onBlur={e => e.target.placeholder = '전화번호'} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, pre: () => Move('nickname'), next: () => Move('role') }) }} onChange={e => setPhoneNumber(e.target.value)} onKeyUp={e => { checkInput(e, '^0[0-9]{2}-[0-9]{4}-[0-9]{4}$', () => setError(''), () => setError('전화번호 형식이 맞지 않습니다.(###-####-####)')); PhoneNumberCheck(e) }} maxLength={13} />
+                <input id='phoneNumber' type='text' className='w-[400px] text-xl mt-[24px]' placeholder='전화번호' onFocus={e => { e.target.placeholder = ''; setFocusing(5) }} onBlur={e => e.target.placeholder = '전화번호'} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, pre: () => Move('nickname'), next: () => Move('role') }) }} onChange={e => {PhoneNumberCheck(e); setPhoneNumber(e.target.value.replaceAll('-', ''))}} onKeyUp={e => {PhoneNumberCheck(e); checkInput(e, '^0[0-9]{2}-[0-9]{4}-[0-9]{4}$', () => setError(''), () => setError('전화번호 형식이 맞지 않습니다.(###-####-####)')); }} maxLength={13} />
                 <select id='role' defaultValue={-1} className='w-[400px] text-xl mt-[24px]' onFocus={() => setFocusing(6)} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, pre: () => Move('phoneNumber'), next: () => Move('birthday') }) }} onChange={e => setRole(Number(e.target.selectedOptions[0].value))}>
                     <option disabled value={-1}>역할을 골라주세요</option>
                     <option value={0}>구매자</option>
                     <option value={1}>판매자</option>
                 </select>
-                <input id='birthday' type='date' className='w-[400px] text-xl mt-[24px]' placeholder='생년월일' onFocus={e => { e.target.placeholder = ''; setFocusing(7) }} onBlur={e => e.target.placeholder = '전화번호'} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, pre: () => Move('role'), next: () => Move('male') }) }} onChange={e => setBirthday(e.target.value)} />
                 <div className='flex w-[120px] justify-between mt-[24px]'>
                     <input type='radio' id='male' name='gender' onFocus={() => setFocusing(8)} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, pre: () => Move('birthday'), next: () => document.getElementById('submit')?.click() }) }} onChange={() => setGender(0)} /><label>남성</label>
                     <input type='radio' id='female' name='gender' onFocus={() => setFocusing(8)} onKeyDown={e => { KeyDownCheck({ preKey, setPreKey, e: e, pre: () => Move('male'), next: () => document.getElementById('submit')?.click() }) }} onChange={() => setGender(1)} /><label>여성</label>
