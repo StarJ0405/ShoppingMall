d543cfe6da7ed57c05d088652f1b69575f7bd081
2ndsprout <ralphlauren5243@gmail.com>
2024-06-17 10:32:05 +0900

    wishController fixed and multiService code clean up

--- backend/src/main/java/com/team/shopping/Controllers/WishController.java
+++ backend/src/main/java/com/team/shopping/Controllers/WishController.java
@@ -14,60 +14,65 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class WishController {
+
     private final MultiService multiService;
-    @GetMapping("/wishList")
-    public ResponseEntity<?> wishList(@RequestHeader("Authorization") String accessToken) {
+
+    @GetMapping("/wishList/check")
+    public ResponseEntity<?> wishCheck(@RequestHeader("Authorization") String accessToken,
+                                       @RequestHeader("ProductId") Long product_id) {
         TokenRecord tokenRecord = this.multiService.checkToken(accessToken);
         if (tokenRecord.isOK()) {
             String username = tokenRecord.username();
-            List<ProductResponseDTO> wishListResponseDTO = this.multiService.getWishList(username);
-            return tokenRecord.getResponseEntity(wishListResponseDTO);
+            boolean check = this.multiService.checkWishList(username,product_id);
+            return tokenRecord.getResponseEntity(check);
         }
         return tokenRecord.getResponseEntity();
     }
+
     @PostMapping("/wishList")
     public ResponseEntity<?> addWishList(@RequestHeader("Authorization") String accessToken,
                                          @RequestBody ProductRequestDTO productRequestDTO) {
         TokenRecord tokenRecord = this.multiService.checkToken(accessToken);
-            try {
-                if (tokenRecord.isOK()) {
-                    String username = tokenRecord.username();
-                    List<ProductResponseDTO> wishListResponseDTO = this.multiService.addToWishList(username, productRequestDTO);
-                    return tokenRecord.getResponseEntity(wishListResponseDTO);
-                }
-            }catch (IllegalArgumentException e) {
-                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("이미 추가한 상품");
+        try {
+            if (tokenRecord.isOK()) {
+                String username = tokenRecord.username();
+                List<ProductResponseDTO> wishListResponseDTO = this.multiService.addToWishList(username, productRequestDTO);
+                return tokenRecord.getResponseEntity(wishListResponseDTO);
             }
-            return tokenRecord.getResponseEntity();
+        }catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("이미 추가한 상품");
         }
-        @DeleteMapping("/wishList")
-        public ResponseEntity<?> deleteToWishList(@RequestHeader("Authorization") String accessToken,
-                @RequestHeader("productId") Long productId) {
-            TokenRecord tokenRecord = this.multiService.checkToken(accessToken);
-                try {
-                    if (tokenRecord.isOK()) {
-                        String username = tokenRecord.username();
-                        List<ProductResponseDTO> wishListResponseDTO = this.multiService.deleteToWishList(username, productId);
-                        return tokenRecord.getResponseEntity(wishListResponseDTO);
-                    }
-                }catch (IllegalArgumentException e) {
-                    return ResponseEntity.status(HttpStatus.FORBIDDEN).body("이미 지워졌거나 없는 상품");
-                }
-                return tokenRecord.getResponseEntity();
+        return tokenRecord.getResponseEntity();
+    }
+    @DeleteMapping("/wishList")
+    public ResponseEntity<?> deleteToWishList(@RequestHeader("Authorization") String accessToken,
+                                              @RequestHeader("productId") Long productId) {
+        TokenRecord tokenRecord = this.multiService.checkToken(accessToken);
+        try {
+            if (tokenRecord.isOK()) {
+                String username = tokenRecord.username();
+                List<ProductResponseDTO> wishListResponseDTO = this.multiService.deleteToWishList(username, productId);
+                return tokenRecord.getResponseEntity(wishListResponseDTO);
             }
-            @DeleteMapping("/wishList/multi")
-            public ResponseEntity<?> deleteMultipleToWishList (@RequestHeader("Authorization") String accessToken,
-                    @RequestHeader("productIdList") List<Long> productIdList) {
-                TokenRecord tokenRecord = this.multiService.checkToken(accessToken);
-                try {
-                    if (tokenRecord.isOK()) {
-                        String username = tokenRecord.username();
-                        List<ProductResponseDTO> wishListResponseDTO = this.multiService.deleteMultipleToWishList(username, productIdList);
-                        return tokenRecord.getResponseEntity(wishListResponseDTO);
-                    }
-                }catch (IllegalArgumentException e) {
-                    return ResponseEntity.status(HttpStatus.FORBIDDEN).body("이미 지워졌거나 없는 상품");
-                }
-                return tokenRecord.getResponseEntity();
-            }
+        }catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("이미 지워졌거나 없는 상품");
         }
+        return tokenRecord.getResponseEntity();
+    }
+    @DeleteMapping("/wishList/multi")
+    public ResponseEntity<?> deleteMultipleToWishList (@RequestHeader("Authorization") String accessToken,
+                                                       @RequestHeader("productIdList") List<Long> productIdList) {
+        TokenRecord tokenRecord = this.multiService.checkToken(accessToken);
+        try {
+            if (tokenRecord.isOK()) {
+                String username = tokenRecord.username();
+                List<ProductResponseDTO> wishListResponseDTO = this.multiService.deleteMultipleToWishList(username, productIdList);
+                return tokenRecord.getResponseEntity(wishListResponseDTO);
+            }
+        }catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("이미 지워졌거나 없는 상품");
+        }
+        return tokenRecord.getResponseEntity();
+    }
+}
+
